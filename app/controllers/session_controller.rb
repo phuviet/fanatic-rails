3e4bbdb5b18899e001361b8071f53a040519b721
PHU VIET <username@users.noreply.github.com>
2017-08-18 17:35:59 +0700

    model update v3

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,8 @@ class SessionController < ApplicationController
     @user = Authentication.find_by(uid: params[:uid], password_digest: params[:password])
 
     if @user
-      # @access_token = request.headers['Content-Type']
+      binding.pry
+      @access_token = request.headers['Access-Token']
       render json: { user: @user, access_token: @access_token }
     else
     end
