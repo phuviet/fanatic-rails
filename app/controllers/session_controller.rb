563b2cb2a583b59fb8b5ac398deddcc2992fc05b
PHU VIET <username@users.noreply.github.com>
2017-08-21 15:06:05 +0700

    login-logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -17,4 +17,23 @@ class SessionController < ApplicationController
     render json: { error: 'Invalid email!' }, status: :not_found
   end
 end
+
+def destroy
+  @provider = request.headers['Provider']
+  @uid = request.headers['Uid']
+  @access_token = request.headers['Access-Token']
+  binding.pry
+  @auth = Authentication.find_by(provider: @provider, uid: @uid)
+  if @auth
+    @tokens = JSON.parse(@auth.access_token)['token']
+    if @tokens.select!{ |token| token == @access_token}
+      @auth.update_attribute(:access_token, '{"token": ' + @tokens.to_s + '}')
+    else
+      render json: { error: 'Invalid access token!' }
+      binding.pry
+    end
+  else
+    render json: { error: 'Invalid user logout!' }
+  end
+end
 end
