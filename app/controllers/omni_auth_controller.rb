7ca9523db1d9da8204feaf6b721a863249b5b30d
PHU VIET <username@users.noreply.github.com>
2017-08-22 11:51:51 +0700

    update login-logout v3

--- app/controllers/omni_auth_controller.rb
+++ app/controllers/omni_auth_controller.rb
@@ -4,7 +4,7 @@ class OmniAuthController < ApplicationController
     auth = Authentication.sign_in_with_account_social(authSocial)
     if auth
       response_headers(authSocial.credentials.token, authSocial.provider, authSocial.uid)
-      render json: { user: auth.user }, status: :success
+      render json: auth.user, status: :success
     else
       render json: { error: 'Invalid social account' }, status: :not_found
     end
