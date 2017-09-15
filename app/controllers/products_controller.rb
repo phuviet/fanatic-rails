d010f849ed49c25c92d543051da86a468e7d1ff8
QUANDDO <dhquan1910@gmail.com>
2017-09-15 16:52:06 +0700

    check

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   def index
     params[:page] = params[:page].to_i > 0 ? params[:page].to_i : 1
     @count = Category.find_by(title: params[:title].capitalize).products.where(brand_id: params[:brand_id].to_i > 0 ? params[:brand_id].to_i : CategoryBrand.select(:brand_id).where(category_id: Category.find_by(title: params[:title]).id)).count
-    @products = Category.find_by(title: params[:title].capitalize).products.where(brand_id: params[:brand_id].to_i > 0 ? params[:brand_id].to_i : CategoryBrand.select(:brand_id).where(category_id: Category.find_by(title: params[:title]).id)).limit(8).offset((params[:page] - 1) * 8)
+    @products = Category.find_by(title: params[:title].capitalize).products.where(brand_id: params[:brand_id].to_i > 0 ? params[:brand_id].to_i : CategoryBrand.select(:brand_id).where(category_id: Category.find_by(title: params[:title]).id)).limit(10).offset((params[:page] - 1) * 10)
     render json:  @products, each_serializer: Product::DetailSerializer, include: [:shop, :category, { properties: :images }], meta: { count: @count }
   end
 
